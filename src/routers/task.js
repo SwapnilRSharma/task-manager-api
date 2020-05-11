0a02975c34b0caab374259b8522a9bae6e736c37
Swapnil Sharma <swapnilrsharma2806@gmail.com>
2020-05-11 11:54:32 +0530

    Setup pagination

--- src/routers/task.js
+++ src/routers/task.js
@@ -20,6 +20,9 @@ router.post('/tasks', auth, async (req, res) => {
 
 })
 
+
+// GET /tasks?completed=true
+//GET /tasks?limit=10&skip=10
 router.get('/tasks', auth, async (req, res) => {
     const match = {}
 
@@ -30,7 +33,11 @@ router.get('/tasks', auth, async (req, res) => {
     try {
         await req.user.populate({
             path: 'tasks',
-            match: match
+            match: match,
+            options: {
+                limit: parseInt(req.query.limit),
+                skip: parseInt(req.query.skip)
+            }
         }).execPopulate()
         res.send(req.user.tasks)
     } catch (e) {
